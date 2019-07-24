7b8175b0363fdecee84c1ef629cc2c56f41a9aa4
kartikdutt18 <kartikdutt@live.in>
2019-07-24 11:31:32 +0530

    LL

--- Merge_Sort_Linked_List.cpp
+++ Merge_Sort_Linked_List.cpp
@@ -16,18 +16,40 @@ struct Node
     }
 };
 
-int len(Node* root){
-    int c=0;
-    while(root){
-        c++;
-        root=root->next;
+
+void PartitionMiddle(Node* root,Node* &ini,Node* &ini2){
+    if(root==NULL) return ;
+    Node* fast=root->next;
+    Node*slow=root;
+    while(fast->next && fast->next->next){
+        fast=fast->next->next;
+        slow=slow->next;
     }
-    return c;
+    ini=root;
+    ini2=slow->next;
+    slow->next=NULL;
 }
 
-Node* GetMiddle(Node* root){
-    if(root==NULL) return root;
-    Node* fast=root;
-    Node*slow=fast;
-    
+Node*sortedMerge(Node * a,Node*b){
+    Node* ans=NULL;
+    if(a==NULL) return b;
+    if(b==NULL) return a;
+    if(a->data<b->data){
+        ans=a;
+        ans->next=sortedMerge(a->next,b);
+    }
+    else{
+        ans=b;
+        ans->next=sortedMerge(a,b->next);
+    }
+    return ans;
+}
+void MergeSort(Node * &head){
+    Node* a;
+    Node* b;
+    if(head==NULL || head->next==NULL) return;
+    PartitionMiddle(head,a,b);
+    MergeSort(a);
+    MergeSort(b);
+    head=sortedMerge(a,b);
 }
