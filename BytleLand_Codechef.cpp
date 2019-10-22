da9b3482f70ba5db88404d9d6416488cefa02b8f
kartikdutt18 <kartikdutt@live.in>
2019-10-22 07:39:50 +0530

    Bitmasking

---
+++ BytleLand_Codechef.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+#include <bits/stdc++.h>
+using namespace std;
+map<int, long long> dp;
+long long optcoins(int n)
+{
+    if (n < 12)
+        return n;
+    if (dp.count(n))
+        return dp[n];
+    dp[n] = optcoins(n / 2) + optcoins(n / 3) + optcoins(n / 4);
+    return dp[n];
+}
+int main()
+{
+    int x;
+    while (scanf("%d", &x) != EOF)
+    {
+
+        cout << optcoins(x) << endl;
+    }
+
+    return 0;
+}
