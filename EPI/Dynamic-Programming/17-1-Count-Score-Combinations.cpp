3e311e8182f367e22cdef5a5df7a8d80235772b5
kartikdutt18 <kartikdutt@live.in>
2020-01-05 12:57:17 +0530

    EPI/DP

--- EPI/Dynamic-Programming/17-1-Count-Score-Combinations.cpp
+++ EPI/Dynamic-Programming/17-1-Count-Score-Combinations.cpp
@@ -5,19 +5,7 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-// In O(S*N)/O(S*N)
-int countCombinations(long long n)
-{
 
-    return 0;
-
-}
-// In O(S*N)/O(S)
-int countCombinationsOpimized(long long n)
-{
-
-    return 0;
-}
 int main()
 {
     int t;
