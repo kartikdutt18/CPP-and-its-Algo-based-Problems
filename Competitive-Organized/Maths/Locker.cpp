5745e6c26093018876b41e81addbc06627a253b9
kartikdutt18 <kartikdutt@live.in>
2020-02-08 21:38:46 +0530

    Math

---
+++ Competitive-Organized/Maths/Locker.cpp
@@ -0,0 +1,15 @@
+//https://www.spoj.com/problems/LOCKER/
+#include<iostream>
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() {
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    
+  }
+  return 0;
+}
